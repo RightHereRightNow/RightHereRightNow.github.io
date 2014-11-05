47c02368e83cc05f8273a8b480293f62519268d3
Matthias Untergassmair <munter2@uic.edu>
2014-11-05 12:29:25 -0600

    Added function to create buttons

--- APP/framework/drawUI.js
+++ APP/framework/drawUI.js
@@ -1,53 +1,80 @@
+var viewBoxWidth = 1000;
+var viewBoxMenuHeight = 1000;
+var viewBoxMapControlHeight = 360;
+
+var button1height = 200;
+var button1width = 900;
+
+var zoomButtonSize = 100;
+var zoomButtonMargin = 20;
+
+var textpadding = 50;
+var font1size = 100;
+
+var linewidth = 10;
+var linepadding = 20;
+
+var button1dx = 50;
+var button1dy = 50;
+
+
+
+function createLevel1Button(svg,yOffset,str,fOnClick) {
+	
+
+	var g = svg.append("svg:g")
+		.attr("fill","green")
+		.attr("class","level1button")
+		.attr("transform","translate(0," + yOffset + ")")
+
+	g.append("rect")
+		.attr("x",button1dx).attr("y",button1dy)
+		.attr("width",button1width).attr("height",button1height)
+		.on("click", function() { 
+			fOnClick();	
+		});
+
+	g.append("svg:text")
+		.attr("fill","blue")
+		// .attr("class","buttontext")
+		.attr("transform","translate(" + (button1dx+textpadding) + "," + (font1size+button1dy+(button1height-font1size)/2) + ")")
+		.text(str)
+		.attr("text-anchor","bottom")
+		.attr("font-size", font1size)
+		.attr("cursor","default")
+
+}
+
+
+
 function drawUI() {
 
 	console.log('Draw UI');
 
-	var viewBoxWidth = 1000;
-	var viewBoxMenuHeight = 1000;
-	var viewBoxMapControlHeight = 360;
-
-	var button1height = 200;
-	var button1width = 900;
-	var button1dx = 50;
-	var button1dy = 50;
-
-	var zoomButtonSize = 100;
-	var zoomButtonMargin = 20;
-
-	var textpadding = 50;
-	var font1size = 100;
-
-	var linewidth = 10;
-	var linepadding = 20;
 
 
 
 	// MENU
-	
+
 	var svgmenu = d3.select("#divmenu").append("svg:svg")
 		.attr("id","menu")
 		.attr("viewBox", "0 0 " + viewBoxWidth + " " + viewBoxMenuHeight)
 		.attr("preserveAspectRatio", "xMinYMin meet")
 		.attr("width",viewBoxWidth).attr("height",viewBoxMenuHeight)
 
-	var g = svgmenu.append("svg:g")
-		.attr("fill","green")
-		.attr("class","level1button")
+	// Selection Button
 
-	g.append("rect")
-		.attr("x",button1dx).attr("y",button1dy)
-		.attr("width",button1width).attr("height",button1height)
-		.on("click", function() { 
-			context.setMode(context.modes.SELECTION)
-		});
+	function setSelection() { context.setMode(context.modes.SELECTION); }
+	function setTrafficLayer() { context.setMode(context.modes.TRAFFICLAYER); }
+	function setCrimeLayer() { context.setMode(context.modes.CRIMELAYER); }
 
-	g.append("svg:text")
-		.attr("fill","blue")
-		// .attr("class","buttontext")
-		.attr("transform","translate(" + (button1dx+textpadding) + "," + (button1dy+font1size) + ")")
-		.text("Selection")
-		.attr("text-anchor","bottom")
-		.attr("font-size", font1size)
+	createLevel1Button(svgmenu,0,"Selection",setSelection)
+	createLevel1Button(svgmenu,(button1height),"Traffic Layer",setTrafficLayer)
+	createLevel1Button(svgmenu,(2*button1height),"Crime Layer",setCrimeLayer)
+	
+
+
+
 
 
 
@@ -105,6 +132,5 @@ function drawUI() {
 		.attr("x2",zoomButtonSize-linepadding).attr("y2",zoomButtonSize/2)
 		.attr("stroke","#444")
 		.attr("stroke-width",linewidth)
-
 
 }
