848121e1b763be576540a3ece00bbd1e758e2d80
Matthias Untergassmair <munter2@uic.edu>
2014-11-05 23:50:47 -0600

    UI update: map control layout almost complete - layer menu still under construction...

--- APP/framework/drawUI.js
+++ APP/framework/drawUI.js
@@ -1,18 +1,18 @@
 var viewBoxWidth = 1000;
 var viewBoxMenuHeight = 1000;
-var viewBoxMapControlHeight = 360;
+var viewBoxMapControlHeight = 240;
 
 var button1height = 200;
 var button1width = 900;
 
-var zoomButtonSize = 100;
+var zoomButtonSize = 200;
 var zoomButtonMargin = 20;
 
 var textpadding = 50;
 var font1size = 100;
 
 var linewidth = 10;
-var linepadding = 20;
+var linepadding = 40;
 
 var button1dx = 50;
 var button1dy = 50;
@@ -26,13 +26,13 @@ function createLevel1Button(svg,yOffset,str,fOnClick) {
 		.attr("fill","green")
 		.attr("class","level1button")
 		.attr("transform","translate(0," + yOffset + ")")
+		.on("click", function() { 
+			fOnClick();	
+		});
 
 	g.append("rect")
 		.attr("x",button1dx).attr("y",button1dy)
 		.attr("width",button1width).attr("height",button1height)
-		.on("click", function() { 
-			fOnClick();	
-		});
 
 	g.append("svg:text")
 		.attr("fill","blue")
@@ -67,10 +67,12 @@ function drawUI() {
 	function setSelection() { context.setMode(context.modes.SELECTION); }
 	function setTrafficLayer() { context.setMode(context.modes.TRAFFICLAYER); }
 	function setCrimeLayer() { context.setMode(context.modes.CRIMELAYER); }
+	function setPlacesOfInterestLayer() { context.setMode(context.modes.PLACESOFINTEREST); }
 
 	createLevel1Button(svgmenu,0,"Selection",setSelection)
 	createLevel1Button(svgmenu,(button1height),"Traffic Layer",setTrafficLayer)
 	createLevel1Button(svgmenu,(2*button1height),"Crime Layer",setCrimeLayer)
+	createLevel1Button(svgmenu,(3*button1height),"Places of Interest",setPlacesOfInterestLayer)
 	
 
 
@@ -87,19 +89,29 @@ function drawUI() {
 		.attr("preserveAspectRatio", "xMinYMin meet")
 		.attr("width",viewBoxWidth).attr("height",viewBoxMapControlHeight)
 		.attr("background-color","blue")
-	
+
+
+	// ZoomIn Button
 	var gZoomIn = svgmapcontrol.append("svg:g")
 		.attr("transform","translate(" + (viewBoxWidth-zoomButtonSize-zoomButtonMargin) + "," + (zoomButtonMargin) + ")")
-		.attr("fill","magenta")
+		.style("fill","transparent")
 		// .attr("class","level1button")
 		.on("click", function() { 
 			map.zoomIn(1);
+		})
+		.on("mouseover", function() {
+			d3.select(this).style("fill","#3db7e4")
+				.attr("stroke-width",2*linewidth) // TODO: implement
+		})
+		.on("mouseout", function() {
+			d3.select(this).style("fill","transparent");
 		});
 
-	gZoomIn.append("rect")
-		// .attr("fill","#3db7e4")
-		.attr("x",0).attr("y",0)
-		.attr("width",zoomButtonSize).attr("height",zoomButtonSize)
+	gZoomIn.append("circle")
+		.attr("transform","translate(" + (zoomButtonSize/2) + "," + (zoomButtonSize/2) + ")")
+		.attr("r",zoomButtonSize/2)
+		.attr("stroke","#444")
+		.attr("stroke-width",linewidth)
 	
 	gZoomIn.append("line")
 		.attr("x1",zoomButtonSize/2).attr("y1",linepadding)
@@ -114,23 +126,108 @@ function drawUI() {
 		.attr("stroke-width",linewidth)
 
 
+	// ZoomOut Button
 	var gZoomOut = svgmapcontrol.append("svg:g")
-		.attr("transform","translate(" + (viewBoxWidth-zoomButtonSize-zoomButtonMargin) + "," + (zoomButtonSize + 2*zoomButtonMargin) + ")")
-		.attr("fill","magenta")
+		.attr("transform","translate(" + (viewBoxWidth-2*zoomButtonSize-2*zoomButtonMargin) + "," + (zoomButtonMargin) + ")")
+		.attr("fill","transparent")
 		// .attr("class","level1button")
 		.on("click", function() { 
 			map.zoomOut(1);
+		})
+		.on("mouseover", function() {
+			d3.select(this).style("fill","#3db7e4");
+		})
+		.on("mouseout", function() {
+			d3.select(this).style("fill","transparent");
 		});
 
-	gZoomOut.append("rect")
-		// .attr("fill","#3db7e4")
+	gZoomOut.append("circle")
+		.attr("transform","translate(" + (zoomButtonSize/2) + "," + (zoomButtonSize/2) + ")")
 		.attr("x",0).attr("y",0)
-		.attr("width",zoomButtonSize).attr("height",zoomButtonSize)
+		.attr("r",zoomButtonSize/2)
+		.attr("stroke","#444")
+		.attr("stroke-width",linewidth)
 
 	gZoomOut.append("line")
 		.attr("x1",linepadding).attr("y1",zoomButtonSize/2)
 		.attr("x2",zoomButtonSize-linepadding).attr("y2",zoomButtonSize/2)
 		.attr("stroke","#444")
 		.attr("stroke-width",linewidth)
+
+	
+	// Toggle SatelliteView Button
+	var toggler = true;
+	
+	var gSatelliteView = svgmapcontrol.append("svg:g")
+		.attr("transform","translate(" + (zoomButtonMargin) + "," + (zoomButtonMargin) + ")")
+		.style("stroke-width","10")
+		// .attr("class","level1button")
+		.on("click", function() { 
+			// map.zoomOut(1);
+			// TODO: change layer
+			// should be possible using addLayer().
+			if(toggler /*map.satelliteLayer()*/) {
+				toggler = false;
+				console.log("TODO: remove satellite Layer");
+				appendSatelliteRect();
+			} else {
+				toggler = true;
+				console.log("TODO: add satellite Layer");
+				appendSatelliteRect();
+			}
+
+			console.log("Clicked!");
+		})
+		.on("mouseover", function() {
+			d3.select(this).style("stroke","magenta")
+		})
+		.on("mouseout", function() {
+			d3.select(this).style("stroke","none")
+		});
+
+	gSatelliteView.append('defs')
+	  .append('pattern')
+		.attr('id', 'imgsatellite')
+		.attr('patternUnits', 'userSpaceOnUse')
+		.attr('width', 2.1*zoomButtonSize)
+		.attr('height', 2.1*zoomButtonSize)
+		.append("svg:image")
+			.attr("xlink:href", "img/chicagosatellite.png")
+			.attr("width", 2.1*zoomButtonSize)
+			.attr("height", 2.1*zoomButtonSize)
+			.attr("transform","translate(0," + (-.5*zoomButtonSize) + ")")
+	
+	gSatelliteView.append('defs')
+	  .append('pattern')
+		.attr('id', 'imgmap')
+		.attr('patternUnits', 'userSpaceOnUse')
+		.attr('width', 2.1*zoomButtonSize)
+		.attr('height', 2.1*zoomButtonSize)
+		.append("svg:image")
+			.attr("xlink:href", "img/chicagomap.png")
+			.attr("width", 2.1*zoomButtonSize)
+			.attr("height", 2.1*zoomButtonSize)
+			.attr("transform","translate(0," + (-.5*zoomButtonSize) + ")")
+
+
+	function appendSatelliteRect() {
+	
+		gSatelliteView.selectAll("rect").remove();
+		
+		gSatelliteView.append("rect")
+			.attr("x",0).attr("y",0)
+			.attr("width",2*zoomButtonSize).attr("height",zoomButtonSize)
+			.attr("fill", function() {
+				if(toggler) {
+					return 'url(#imgsatellite)';
+				} else {
+					return 'url(#imgmap)';
+				}
+			});
+	
+	}
+
+	appendSatelliteRect();
+
 
 }
