938b384455a43e29ba5c7f36e0279f04214bbb9c
krbalmryde <kyle.almryde@gmail.com>
2014-11-09 13:58:37 -0600

    adding unpushed changes...why they were not added I do not know...sorry guys\!

---
+++ APP/framework/markers/AbandonedVehicleMarker.js
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////
+//           Abandoned Vehicle Marker object
+//////////////////////////////////////////////////////////////
+/*
+    data: Json Object which assumes the following fields
+        service_request_number,
+        creation_date,  // aka date vehicle was reported missing
+        vehicle_make_model,
+        vehicle_color,
+        latitude,
+        longitude
+
+ */
+
+var AbandonedVehicleMarker = function(data) {
+    this.service_request_number = data.service_request_number;
+    this.creation_date = data.creation_date;
+    this.vehicle_make_model = data.vehicle_make_model;
+    this.vehicle_color =  data.vehicle_color;
+
+    var latlng = L.latLng(data.latitude, data.longitude);
+
+    var popupstr = "<p>Request #: " + this.service_request_number + "</p>" +
+                   "<p>Date Stolen: " + this.creation_date + "</p>" +
+                   "<p>Vehicle Make/Model: " + this.vehicle_make_model + "</p>" +
+                   "<p>Vehicle Color: " + this.vehicle_color + "</p>";
+
+
+    var iconOld = L.AwesomeMarkers.icon({
+        icon: "car",
+        spin:false,
+        markerColor: "gray",
+        iconColor: "white"
+    });
+
+    var iconNew = L.AwesomeMarkers.icon({
+        icon: "car",
+        spin:true,
+        markerColor: "lightgray",
+        iconColor: "white"
+    });
+
+    this.setIconNew(iconNew);
+    this.setIconOld(iconOld);
+    this.setLatLng(latlng);
+    this.setPopupString(popupstr);
+    this.viewNewIcon();
+};
