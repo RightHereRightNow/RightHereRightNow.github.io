3d191a094390e8a64cb73cb373dbc697d2010b63
krishnaBharadwaj <kbhara5@uic.edu>
2014-11-23 22:30:57 -0600

    Graphs hide and show

--- APP/framework/BarChart.js
+++ APP/framework/BarChart.js
@@ -17,6 +17,14 @@ function BarChart (svg){
 		bottom: 70 
 	};
 	this.svg.attr("viewBox","0 0 160 90");
+	var width = parseFloat(svg.style("width")) ;
+	var height = parseFloat(svg.style("height")) ;
+	this.svg.append("rect")
+		.attr("x", 0)
+	    .attr("y", 0)
+        .attr("width", width)
+        .attr("height", height)
+        .attr("fill", "rgba(68,68,68,0.9)");
 	
 }
 
